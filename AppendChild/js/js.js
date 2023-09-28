2a5c1d18c4c2a7b9d3e33cd99cf096e5ea3bef45
Luiza2112 <ritafilhotes4@gmail.com>
2023-09-28 19:22:50 -0300

    Feat: Adicionei o exercício de AppendChild.

--- AppendChild/js/js.js
+++ AppendChild/js/js.js
@@ -6,28 +6,28 @@ function Cadastrar() {
     };
 
     // Criar um novo elemento LI:
-    const lista_item = document.createElement("li");
+    const lista_item = document.createElement("li")
 
     // Adicionar conteúdo nesse elemento:
-    const prod_nome = document.createElement("h3");
-    const prod_marca = document.createElement("h4");
-    const prod_preco = document.createElement("p");
+    const prod_nome = document.createElement("h3")
+    const prod_marca = document.createElement("h4")
+    const prod_preco = document.createElement("p")
 
     prod_nome.innerHTML = item.nome;
     prod_marca.innerHTML = item.marca;
     prod_preco.innerHTML = item.preco;
 
     // Criar um novo elemento DIV para o bloco com cor:
-    const corBlock = document.createElement("div");
-    corBlock.classList.add("cor-block"); 
-    lista_item.appendChild(corBlock);
+    const corBlock = document.createElement("div")
+    corBlock.classList.add("cor-block")
+    lista_item.appendChild(corBlock)
 
     // Adicionar os elementos ao LI:
-    lista_item.appendChild(prod_nome);
-    lista_item.appendChild(prod_marca);
-    lista_item.appendChild(prod_preco);
+    lista_item.appendChild(prod_nome)
+    lista_item.appendChild(prod_marca)
+    lista_item.appendChild(prod_preco)
 
     // Adicionar esse LI no UL:
-    document.getElementById("lista_prod").appendChild(lista_item);
-    console.log(lista_item);
+    document.getElementById("lista_prod").appendChild(lista_item)
+    console.log(lista_item)
 }
