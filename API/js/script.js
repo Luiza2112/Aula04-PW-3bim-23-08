b2c29c12c92e8ba38b5db257faea4a0d79c0bd68
Luiza2112 <ritafilhotes4@gmail.com>
2023-11-02 11:43:32 -0300

    feat:tentei adicionar mais uma api.

--- API/js/script.js
+++ API/js/script.js
@@ -5,8 +5,21 @@ fetch('https://images.dog.ceo/breeds/segugio-italian/n02090722_002.jpg')
         const imgURL = URL.createObjectURL(blob)
         const img = '<img src="' + imgURL + '" />'
 
-        document.querySelector('#cachorro').innerHTML = img
-    })
+       document.querySelector('#cachorro').innerHTML = img
+   })
 
 //const cachorro = document.getElementById("cachorro")
 //cachorro.src = "https://images.dog.ceo/breeds/segugio-italian/n02090722_002.jpg"
+
+//const gato = "https://api.thecatapi.com/v1/"
+//const imgGato = document.getElementById('imgGato');
+//const getGato = document.getElementById('getGato');
+//const btnGetApiData = document.getElementById('btnGetApiData')
+
+//function getImg(){
+//    fetch(gato + "images/search").then( response => response.json()).then( data => imgGato.src = data.message)
+//}
+
+//function getApiData(){
+//    fetch(gato + "images/search").then(response => {console.log(response)})
+//}
