ca4b115f3ab49261d8b22be1e95e9a6c7f37289d
Luiza2112 <ritafilhotes4@gmail.com>
2023-11-20 12:54:25 -0300

    feat: adicionado novo exercício

--- API/Exemplo_aula/js/script.js
+++ API/Exemplo_aula/js/script.js
@@ -30,6 +30,31 @@ function showPokemonList(){
     })
 }
 
+function showSixPokemons(){
+    let listaPokemons = []
+    fetch(BASEURL + '?limit=6').then( Response => {
+        if (Response.status == 200){
+            listaPokemons = Response.json().then( json => {json.results.map( pokemon => { 
+                let liPokemon = document.createElement("li")
+                let liPokemonImg = document.createElement("img")
+                fetch(pokemon.url).then(Response => { pokemonImg = Response.json().then( (pokemon) => {(liPokemonImg.src = pokemon.sprites.front_default)})})
+                liPokemon.innerHTML = pokemon.name
+                liPokemon.appendChild(liPokemonImg)
+                document.getElementById("listaPokemons").appendChild(liPokemon)
+            })
+            return json})
+            return listaPokemons
+        }
+    })
+}
+
+function showPokemonById(){
+    fetch(BASEURL + 5).then((Response) => {
+        Response.json().then((pokemon) => {
+            console.log(pokemon.name)
+        })})
+}
+
 /*
 Chamar lista da API no console: "getPokemonList()"
 
