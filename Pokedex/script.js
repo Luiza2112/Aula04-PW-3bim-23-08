d57f87a5e5eda37fa1a2d7226eccee873b1e0ba8
Luiza2112 <ritafilhotes4@gmail.com>
2023-12-01 22:13:13 -0300

    feat: terminei a Pokédex.

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -3,8 +3,16 @@ const BASEURLIMG = "https://raw.githubusercontent.com/PokeAPI/sprites/master/spr
 
 const pokemonName = document.querySelector('.pokemon_name')
 const pokemonNumber = document.querySelector('.pokemon_number')
-//const pokemonImg = document.querySelector('.pokemon_img')
+const pokemonImg = document.querySelector('.pokemon_img')
 
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+const buttonVoltar = document.querySelector('.voltar')
+const buttonProximo = document.querySelector('.proximo')
+
+let searchPokemon = 1
+
+//Procura Pokémon na API
 const fetchPokemon = async (pokemon) => {
 //'await' é apenas para funções assíncronas, e o 'async' define a função como tal.
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -15,13 +23,47 @@ const fetchPokemon = async (pokemon) => {
     }
   }
 
-//Revisar: nome do Pokemon não está aparecendo, não sei o que está errado.
+//Mostra os dados encontrados do Pokémon
 const showPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Procurando...'
+    pokemonNumber.innerHTML = ''
 
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name
-    pokemonNumber.innerHTML = data.id
-    //pokemonImg.src = data['sprites']['versions']['generation']
+    if (data) {
+      pokemonImg.style.display = 'block'
+      pokemonName.innerHTML = data.name
+      pokemonNumber.innerHTML = data.id
+      pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']
+      ['animated']['front_default']
+      input.value = ''
+      searchPokemon = data.id
+    } else {
+        pokemonImg.style.display = 'none'
+        pokemonName.innerHTML = 'Não encontrado'
+        pokemonNumber.innerHTML = ''
+        input.value = ''
+    }
 }
 
+//Pega os dados inseridos no formulario para fazer a procura do Pokémon
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  showPokemon(input.value.toLowerCase())
+})
+
+buttonVoltar.addEventListener('click', () => {
+  if (searchPokemon > 1){
+    searchPokemon -= 1
+    showPokemon(searchPokemon)
+  }
+  
+})
+
+buttonProximo.addEventListener('click', () => {
+  searchPokemon += 1
+  showPokemon(searchPokemon)
+})
+
+showPokemon(searchPokemon)
